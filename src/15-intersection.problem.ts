b634318c3717ce5a5200ed7a03cbf3de4897639c
ahmvdev <ahmadsaeed3290@gmail.com>
2025-07-16 03:12:58 +0500

    all problems finished

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -14,7 +14,9 @@ interface Post {
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): unknown => {
+type DefaultUser= User & {posts: Post[]}
+
+export const getDefaultUserAndPosts = (): DefaultUser => {
   return {
     id: "1",
     firstName: "Matt",
